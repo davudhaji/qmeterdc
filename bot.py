afd8f042769dd75b11574c3f7812c4eea016aea1
Davud <davud.h@cxlab.az>
2023-07-09 11:11:08 +0400

    some commands

--- bot.py
+++ bot.py
@@ -3,21 +3,45 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-import youtube_dl
-
+import time
 load_dotenv()
 
 token = os.getenv('TOKEN')
 print(token,'TOKEN')
-intents = discord.Intents.default()
-intents.typing = False  # Yazma olayları için intents'i devre dışı bırakabilirsiniz
-intents.presences = False  # Durum olayları için intents'i devre dışı bırakabilirsiniz
+intents = discord.Intents().all()
+# intents.members = True
+# intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Bot olarak giriş yapıldı: {bot.user.name}')
+
+@bot.command()
+async def ping(ctx):
+    start_time = time.time()
+    message = await ctx.send("Pinging...")
+    end_time = time.time()
+
+    latency = round((end_time - start_time) * 1000, 2)  # Latency in milliseconds
+
+    await message.edit(content=f"Pong! Bot Latency: {bot.latency * 1000} ms | Member Latency: {latency} ms")
+
+
+
+
+@bot.command()
+async def status(ctx):
+    guild = ctx.guild
+    members = guild.members
+    print()
+    for member in members:
+        message = f"{member.name}: {member.status}"
+        await ctx.send(message)
+
+
 
 @bot.event
 async def on_member_join(member):
@@ -31,30 +55,6 @@ async def on_member_join(member):
 @bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
-
-
-
-@bot.command()
-async def play(ctx, url):
-    voice_channel = ctx.message.author.voice.channel
-    if voice_channel is None:
-        await ctx.send("Önce bir sesli kanala katılmanız gerekiyor.")
-        return
-    voice_channel = await voice_channel.connect()
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        url2 = info['formats'][0]['url']
-        voice_channel.play(discord.FFmpegPCMAudio(url2))
 
 
 
